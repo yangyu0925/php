808e414deff4e55d436254bd520e0db70880a041
yangyu <172964776@qq.com>
2018-03-07 10:12:00 +0800

    commit

--- README.md
+++ README.md
@@ -10,14 +10,14 @@
     
     function array_change_key_case_recursive($arr, $case = CASE_LOWER)
     {
-        return array_map(function($item){
-        
+        return array_map(function($item) use ($case){
+    
             if (is_array($item)) {
-                $item = array_change_key_case_recursive($item);
+                $item = array_change_key_case_recursive($item, $case);
             }
-            
+    
             return $item;
-            
+    
         }, array_change_key_case($arr, $case));
     }
     
--- index.php
+++ index.php
@@ -1,28 +1,18 @@
 <?php
 
-function array_insert(&$arr, $pos, $new_el=null) {
-    $arraypad = array_pad($arr, count($arr)+1, 0);
-    for ($i = count($arraypad) - 1; $i>=$pos; $i--) {
-        $arr[$i] = $arr[$i-1];
-        if ($i == $pos) {
-            $arr[$i] = $new_el;
+$arr = ['name' => 'taylor', 'languages' => ['a' => 'php', 'b' => 'javascript']];
+
+function array_change_key_case_recursive($arr, $case = CASE_LOWER)
+{
+    return array_map(function($item) use ($case){
+
+        if (is_array($item)) {
+            $item = array_change_key_case_recursive($item, $case);
         }
-    }
+
+        return $item;
+
+    }, array_change_key_case($arr, $case));
 }
 
-$digits = array();
-$digits[0] = 0;
-$digits[1] = 1;
-$digits[2] = 2;
-$digits[3] = 3;
-$digits[4] = 4;
-$digits[5] = 5;
-
-array_insert($digits, 3, 100);
-echo "new: "; var_dump($digits);
-
-
-
-
-
-
+var_dump(array_change_key_case_recursive($arr, CASE_UPPER));
