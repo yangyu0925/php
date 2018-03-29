f3acd955a8186131453107f8e013e52a664f8884
yangyu <172964776@qq.com>
2018-03-29 14:15:53 +0800

    commit

--- index.php
+++ index.php
@@ -16,3 +16,18 @@
 //var_dump($stmt->fetch(PDO::FETCH_ASSOC));
 
 
+try{
+    $db = new PDO('mysql:dbname=ik;host=localhost', 'root', '');
+} catch(PDOException $e) {
+    die('数据库连接错误！错误信息：' . $e->getMessage());
+}
+$db->query('SET NAMES utf8');
+
+$sql = "select * from daily_hit_counter where slot = :slot";
+
+$stmt = $db->prepare($sql);
+$stmt->execute(['slot' => '4']);
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    echo $row['day'] . '<br/>';
+}
