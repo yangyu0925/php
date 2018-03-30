b57086ad60a47bd88061ef2856d15ecd6448c50b
yangyu <172964776@qq.com>
2018-03-30 16:12:23 +0800

    commit

--- index.php
+++ index.php
@@ -15,19 +15,45 @@
 //
 //var_dump($stmt->fetch(PDO::FETCH_ASSOC));
 
+class mysql
+{
+    protected $db;
 
-try{
-    $db = new PDO('mysql:dbname=ik;host=localhost', 'root', '');
-} catch(PDOException $e) {
-    die('数据库连接错误！错误信息：' . $e->getMessage());
+    protected $stmt;
+
+    public function __construct($dbname, $host, $user, $password)
+    {
+        $this->db = new PDO("mysql:dbname=$dbname;host=$host", $user, $password);
+        $this->db->query('SET NAMES UTF');
+    }
+
+    public function query($sql, $paramenter)
+    {
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->execute($paramenter);
+
+        $this->stmt = $stmt;
+
+        return $this;
+    }
+
+    public function all()
+    {
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function first()
+    {
+        return $this->stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
-$db->query('SET NAMES utf8');
 
-$sql = "select * from daily_hit_counter where slot = :slot";
+$db = new mysql('bbs', 'localhost', 'root', '');
 
-$stmt = $db->prepare($sql);
-$stmt->execute(['slot' => '4']);
+$sql = "select * from users";
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    echo $row['day'] . '<br/>';
-}
+$all = $db->query($sql, [])->first();
+
+var_dump($all);
+
