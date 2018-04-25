3b353d175b7559670b12b3d24313c71ccf87bfea
yangyu <172964776@qq.com>
2018-04-25 11:22:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -115,3 +115,17 @@ foreach ($values as $key => $value) {
 }
  */
  
+try{
+   $DB = new PDO('mysql:dbname=ik;host=localhost', 'root', '');
+} catch (PDOException $e) {
+   die('数据库连接错误！错误信息：' . $e ->getMessage());
+}
+$DB->query('SET NAMES utf8');
+
+
+$sql = 'select * from daily_hit_counter where slot = :slot';
+
+$stmt = $DB->prepare($sql);
+$stmt->execute(['slot' => 4]);
+
+var_dump($stmt->fetch(PDO::FETCH_ASSOC));
